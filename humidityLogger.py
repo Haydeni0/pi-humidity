0830768d94f439209a569e7ae7b218ba34b156fc
HaydensPiZeroWH <hayden.dorahy@gmail.com>
2021-09-23 20:45:59 +0100

    Fix csv saving, change date format

--- humidityLogger.py
+++ humidityLogger.py
@@ -5,9 +5,11 @@ import Adafruit_DHT
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
+filepath = '/home/pi/pi-humidity/DHT22_data.csv'
+
 try:
-    f = open('/home/pi/Humidity/humidity.csv', 'a+')
-    if os.stat('/home/pi/Humidity/humidity.csv').st_size == 0:
+    f = open(filepath, 'a+')
+    if os.stat(filepath).st_size == 0:
             f.write('Date,Time,Temperature,Humidity\r\n')
 except:
     pass
@@ -16,7 +18,7 @@ while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
     if humidity is not None and temperature is not None:
-        f.write(f"{time.strftime('%y-%m-%d')},{time.strftime('%H:%M')},{temperature:0.2f},{humidity:0.2f}\r\n")
+        f.write(f"{time.strftime('%Y-%m-%d')},{time.strftime('%H:%M:%S')},{temperature:0.2f},{humidity:0.2f}\r\n")
     else:
         print("Failed to retrieve data from humidity sensor")
 
