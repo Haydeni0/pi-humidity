ed4461ff22869ea6242da70fee251253a0117373
Haydeni0 <hayden.dorahy@gmail.com>
2021-09-24 17:33:32 +0100

    Instead of nasty try/except statement, wrap within a block

--- humidityLogger.py
+++ humidityLogger.py
@@ -7,19 +7,17 @@ DHT_PIN = 4
 
 filepath = '/home/pi/pi-humidity/data/DHT22_data.csv'
 
-try:
-    f = open(filepath, 'a+')
+
+with open(filepath, 'a+') as f:
     if os.stat(filepath).st_size == 0:
-            f.write('Date,Time,Temperature,Humidity\r\n')
-except:
-    pass
+        f.write('Date,Time,Temperature,Humidity\r\n')
 
-while True:
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    while True:
+        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
-    if humidity is not None and temperature is not None:
-        f.write(f"{time.strftime('%Y-%m-%d')},{time.strftime('%H:%M:%S')},{temperature:0.2f},{humidity:0.2f}\r\n")
-    else:
-        print("Failed to retrieve data from humidity sensor")
+        if humidity is not None and temperature is not None:
+            f.write(f"{time.strftime('%Y-%m-%d')},{time.strftime('%H:%M:%S')},{temperature:0.2f},{humidity:0.2f}\r\n")
+        else:
+            print("Failed to retrieve data from humidity sensor")
 
-    time.sleep(2)
+        time.sleep(2)
