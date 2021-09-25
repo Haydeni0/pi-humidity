58488a9fb35cd9a65293d8a65875e5038374d7cb
Haydeni0 <hayden.dorahy@gmail.com>
2021-09-25 12:37:21 +0100

    Add comments, remove millisecond resolution

--- humidityLogger.py
+++ humidityLogger.py
@@ -8,15 +8,18 @@ DHT_PIN = 4
 
 filepath = '/home/pi/pi-humidity/data/DHT22_data.csv'
 
-
+# Open csv file (or create)
 with open(filepath, 'a+') as f:
+    # If the file is empty, make the colnames
     if os.stat(filepath).st_size == 0:
         f.write('Datetime,Temperature,Humidity\r\n')
 
     while True:
+        # Read from the sensor
         humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-
+        
         current_time = datetime.datetime.now()
+        current_time = current_time.replace(microsecond = 0) # We don't need microsecond accuracy
 
         if humidity is None and temperature is not None:
             f.write(f"{current_time},{temperature:0.2f},NaN\r\n")
