03aadc538c5c94655448b14709a73e91908045dc
Haydeni0 <hayden.dorahy@gmail.com>
2021-09-25 11:52:34 +0100

    Merge date and time columns to datetime

--- README.md
+++ README.md
@@ -23,9 +23,11 @@ Repo containing code for pi humidity monitoring
 ### Display
 - Pull data from the last two days to display on screen
 - Figure out how to do that...
+  - Use matplotlib.animation
+  - Test using randomly generated time series data on windows (in the same csv format)
 
 ### Fan
-- Solder fan to a usb so it runs on 5v from usb rather than buck converter
+- Solder fan to a usb so it runs on 5v from usb rather than wall plug 5v transformer
 - Or maybe control fan using raspberry pi, regulate humidity
 
 
--- humidityLogger.py
+++ humidityLogger.py
@@ -1,5 +1,6 @@
 import os
 import time
+import datetime
 import Adafruit_DHT
 
 DHT_SENSOR = Adafruit_DHT.DHT22
@@ -10,19 +11,20 @@ filepath = '/home/pi/pi-humidity/data/DHT22_data.csv'
 
 with open(filepath, 'a+') as f:
     if os.stat(filepath).st_size == 0:
-        f.write('Date,Time,Temperature,Humidity\r\n')
+        f.write('Datetime,Temperature,Humidity\r\n')
 
     while True:
         humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
-        
+        current_time = datetime.datetime.now()
+
         if humidity is None and temperature is not None:
-            f.write(f"{time.strftime('%Y-%m-%d')},{time.strftime('%H:%M:%S')},{temperature:0.2f},NaN\r\n")
+            f.write(f"{current_time},{temperature:0.2f},NaN\r\n")
         if humidity is not None and temperature is None:
-            f.write(f"{time.strftime('%Y-%m-%d')},{time.strftime('%H:%M:%S')},NaN,{humidity:0.2f}\r\n")
+            f.write(f"{current_time},NaN,{humidity:0.2f}\r\n")
         if humidity is not None and temperature is not None:
-            f.write(f"{time.strftime('%Y-%m-%d')},{time.strftime('%H:%M:%S')},{temperature:0.2f},{humidity:0.2f}\r\n")
+            f.write(f"{current_time},{temperature:0.2f},{humidity:0.2f}\r\n")
         else:
-            f.write(f"{time.strftime('%Y-%m-%d')},{time.strftime('%H:%M:%S')},NaN,NaN\r\n")
+            f.write(f"{current_time},NaN,NaN\r\n")
 
         time.sleep(2)
