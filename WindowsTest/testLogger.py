47764e54f84aa6e00f99928f2812367ec615bdbe
Haydeni0 <hayden.dorahy@gmail.com>
2021-09-26 12:19:12 +0100

    Backup the test directory
    The stuff in there is still useful

---
+++ WindowsTest/testLogger.py
@@ -0,0 +1,35 @@
+import time
+import datetime
+import os
+import pandas as pd
+import numpy as np
+
+filepath = "WindowsTest/TestData.csv"
+
+humidity = [np.random.normal(66, 2)]
+temperature = [np.random.normal(22, 0.5)]
+
+while True:
+    # Open csv file (or create)
+    f = open(filepath, 'a+')
+    # If the file is empty, make the colnames
+
+    if os.stat(filepath).st_size == 0:
+        f.write('Datetime,Temperature,Humidity\n')
+    # Simulate an AR(1) process
+    humidity.append(0.99*humidity[len(humidity)-1] + np.random.normal(0, 2))
+    temperature.append(0.99*temperature[len(temperature)-1] + np.random.normal(0, 0.5))
+
+    chumidity = humidity[len(humidity)-1]
+    ctemperature = temperature[len(temperature)-1]
+
+    current_time = datetime.datetime.now()
+    # We don't need microsecond accuracy
+    current_time = current_time.replace(microsecond=0)
+
+    f.write(f"{current_time},{ctemperature:0.2f},{chumidity:0.2f}\n")
+
+
+    f.close()
+
+    time.sleep(2)
