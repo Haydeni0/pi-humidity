7c72f0afae2092867efb0a755b8cd26cd91c0e1d
Haydeni0 <hayden.dorahy@gmail.com>
2021-09-26 13:24:14 +0100

    Remove carriage return \r from csv write

--- humidityLogger.py
+++ humidityLogger.py
@@ -10,27 +10,26 @@ filepath = '/home/pi/pi-humidity/data/DHT22_data.csv'
 
 while True:
     # Open csv file (or create)
-    f = open(filepath, 'a+')
-    # If the file is empty, make the colnames
+    with open(filepath, 'a+') as f:
+        # If the file is empty, make the colnames
+        if os.stat(filepath).st_size == 0:
+            f.write('Datetime,Temperature,Humidity\n')
 
-    if os.stat(filepath).st_size == 0:
-        f.write('Datetime,Temperature,Humidity\r\n')
-    # Read from the sensor
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+        # Read from the sensor
+        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
-    current_time = datetime.datetime.now()
-    # We don't need microsecond accuracy
-    current_time = current_time.replace(microsecond=0)
+        current_time = datetime.datetime.now()
+        # We don't need microsecond accuracy
+        current_time = current_time.replace(microsecond=0)
 
-    if humidity is None and temperature is not None:
-        f.write(f"{current_time},{temperature:0.2f},NaN\r\n")
-    if humidity is not None and temperature is None:
-        f.write(f"{current_time},NaN,{humidity:0.2f}\r\n")
-    if humidity is not None and temperature is not None:
-        f.write(f"{current_time},{temperature:0.2f},{humidity:0.2f}\r\n")
-    else:
-        f.write(f"{current_time},NaN,NaN\r\n")
+        if humidity is None and temperature is not None:
+            f.write(f"{current_time},{temperature:0.2f},NaN\n")
+        if humidity is not None and temperature is None:
+            f.write(f"{current_time},NaN,{humidity:0.2f}\n")
+        if humidity is not None and temperature is not None:
+            f.write(f"{current_time},{temperature:0.2f},{humidity:0.2f}\n")
+        else:
+            f.write(f"{current_time},NaN,NaN\n")
 
-    f.close()
 
     time.sleep(2)
