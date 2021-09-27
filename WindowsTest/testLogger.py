158f043e2348fe411aa615ffc9debdd804b7406b
Haydeni0 <hayden.dorahy@gmail.com>
2021-09-27 21:03:02 +0100

    Update test logger to produce two files

--- WindowsTest/testLogger.py
+++ WindowsTest/testLogger.py
@@ -4,29 +4,36 @@ import os
 import pandas as pd
 import numpy as np
 
-filepath = "WindowsTest/TestData.csv"
+filepath_inside = "WindowsTest/TestData_inside.csv"
+filepath_outside = "WindowsTest/TestData_outside.csv"
 
-humidity = [np.random.normal(66, 2)]
-temperature = [np.random.normal(22, 0.5)]
+humidity_inside = [np.random.normal(0, 2)]
+temperature_inside = [np.random.normal(0, 0.5)]
+humidity_outside = [np.random.normal(0, 2)]
+temperature_outside = [np.random.normal(0, 0.5)]
 
 while True:
     # Open csv file (or create)
-    with open(filepath, 'a+') as f:
+    with open(filepath_inside, 'a+') as f_inside, open(filepath_outside, 'a+') as f_outside:
         # If the file is empty, make the colnames
-        if os.stat(filepath).st_size == 0:
-            f.write('Datetime,Temperature,Humidity\n')
+        def updateFile(filepath, f, humidity, temperature):
+            if os.stat(filepath).st_size == 0:
+                f.write('Datetime,Temperature,Humidity\n')
             
-        # Simulate an AR(1) process
-        humidity.append(0.99*humidity[len(humidity)-1] + np.random.normal(0, 2))
-        temperature.append(0.99*temperature[len(temperature)-1] + np.random.normal(0, 0.5))
+            # Simulate an AR(1) process
+            humidity.append(0.92*humidity[len(humidity)-1] + np.random.normal(0, 2))
+            temperature.append(0.92*temperature[len(temperature)-1] + np.random.normal(0, 0.5))
 
-        chumidity = humidity[len(humidity)-1]
-        ctemperature = temperature[len(temperature)-1]
+            chumidity = humidity[len(humidity)-1] + 66 # Process mean of 66
+            ctemperature = temperature[len(temperature)-1] + 22 # Process mean of 22
 
-        current_time = datetime.datetime.now()
-        # We don't need microsecond accuracy
-        current_time = current_time.replace(microsecond=0)
+            current_time = datetime.datetime.now()
+            # We don't need microsecond accuracy
+            current_time = current_time.replace(microsecond=0)
 
-        f.write(f"{current_time},{ctemperature:0.2f},{chumidity:0.2f}\n")
+            f.write(f"{current_time},{ctemperature:0.2f},{chumidity:0.2f}\n")
+        
+        updateFile(filepath_inside, f_inside, humidity_inside, temperature_inside)
+        updateFile(filepath_outside, f_outside, humidity_outside, temperature_outside)
 
-    time.sleep(2)
+    time.sleep(1)
