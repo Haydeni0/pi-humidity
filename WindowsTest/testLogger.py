a67114bb96d42c589f93f6bbf8b3fffb4a57db9e
Haydeni0 <hayden.dorahy@gmail.com>
2021-10-27 15:19:11 +0100

    Update test logger to update the local windows SQL server for (faster) testing & querying

--- WindowsTest/testLogger.py
+++ WindowsTest/testLogger.py
@@ -1,49 +1,79 @@
 import datetime
-import os
-import time
 import math
+import time
 
 import numpy as np
-import pandas as pd
 
-filepath_inside = "WindowsTest/TestData/inside.csv"
-filepath_outside = "WindowsTest/TestData/outside.csv"
+import os
+import sys
+import inspect
 
-humidity_inside = [np.random.normal(0, 2)]
-temperature_inside = [np.random.normal(0, 0.5)]
-humidity_outside = [np.random.normal(0, 2)]
-temperature_outside = [np.random.normal(0, 0.5)]
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
 
+from DHT_MySQL_interface import ObsDHT, DHTConnection
 
-log_interval = 1
-AR_time_constant = 60*60 # Time constant of the AR(1) processes (in seconds)
+TABLE_NAME_inside = "dht_inside"
+TABLE_NAME_outside = "dht_outside"
+
+# Data generation parameters
+# Use same AR parameters for both inside and outside
+log_interval = 1  # in seconds
+AR_time_constant = 60*60  # Time constant of the AR(1) processes (in seconds)
 C = math.exp(-log_interval/AR_time_constant)
-sigma_H = 10
-sigma_T = 4
-mean_H = 66
-mean_T = 22
+sigma_H = 6
+sigma_T = 3
+mean_H_inside = 82
+mean_T_inside = 23
+mean_H_outside = 66
+mean_T_outside = 22
+
+inside_obs = ObsDHT(D=datetime.datetime.now(), H=80, T=20)
+outside_obs = ObsDHT(
+    D=datetime.datetime.now(), H=60, T=20)
+
+
+def updateAR(x0: float, sigma: float, C: float, mean: float) -> float:
+    # Simulate an AR(1) process
+    x1 = C*(x0 - mean) + np.random.normal(0, sigma*math.sqrt(1-C*C)) + mean
+
+    return x1
+
+
+# ---- MySQL functions and connection ----
+
+connection_config = {
+    "host": 'localhost',
+    "database": "pi_humidity",
+    "user": "Haydeni0",
+    "password": "OSzP34,@H0.I2m$sZpI<",
+    'raise_on_warnings': True
+}
+
+
+pi_humidity_SQL = DHTConnection(connection_config)
+
+pi_humidity_SQL.createTable(TABLE_NAME_inside)
+pi_humidity_SQL.createTable(TABLE_NAME_outside)
+
+
 while True:
-    # Open csv file (or create)
-    with open(filepath_inside, 'a+') as f_inside, open(filepath_outside, 'a+') as f_outside:
-        # If the file is empty, make the colnames
-        def updateFile(filepath, f, humidity, temperature):
-            if os.stat(filepath).st_size == 0:
-                f.write('Datetime,Temperature,Humidity\n')
-            
-            # Simulate an AR(1) process
-            humidity.append(C*humidity[len(humidity)-1] + np.random.normal(0, sigma_H*math.sqrt(1-C*C)))
-            temperature.append(C*temperature[len(temperature)-1] + np.random.normal(0, sigma_T*math.sqrt(1-C*C)))
+    # Update DHT observations (using an AR(1))
+    inside_obs = ObsDHT(
+        D=datetime.datetime.now(),
+        H=updateAR(inside_obs.H, sigma_H, C, mean_H_inside),
+        T=updateAR(inside_obs.T, sigma_T, C, mean_T_inside)
+    )
+    outside_obs = ObsDHT(
+        D=datetime.datetime.now(),
+        H=updateAR(outside_obs.H, sigma_H, C, mean_H_outside),
+        T=updateAR(outside_obs.T, sigma_T, C, mean_T_outside)
+    )
 
-            chumidity = humidity[len(humidity)-1] + mean_H
-            ctemperature = temperature[len(temperature)-1] + mean_T
 
-            current_time = datetime.datetime.now()
-            # We don't need microsecond accuracy
-            current_time = current_time.replace(microsecond=0)
-
-            f.write(f"{current_time},{ctemperature:0.2f},{chumidity:0.2f}\n")
-        
-        updateFile(filepath_inside, f_inside, humidity_inside, temperature_inside)
-        updateFile(filepath_outside, f_outside, humidity_outside, temperature_outside)
+    # Send the observation to the server
+    pi_humidity_SQL.sendObservation(TABLE_NAME_inside, inside_obs)
+    pi_humidity_SQL.sendObservation(TABLE_NAME_outside, outside_obs)
 
     time.sleep(log_interval)
