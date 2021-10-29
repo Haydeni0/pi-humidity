66409cfae01fbe388f4e00086ec3249e9e2c2d66
Haydeni0 <hayden.dorahy@gmail.com>
2021-10-29 22:50:15 +0100

    Add python script that plots DHT on the raspberry pi using its local server

---
+++ plotRaspiDHT.py
@@ -0,0 +1,13 @@
+from dhtPlotting import plotDHT
+
+# Call this module from the raspberry pi
+# Plot DHT, connect to the local server
+connection_config = {
+    "host": 'localhost',
+    "database": "pi_humidity",
+    "user": "haydeni0",
+    "password": "raspizeroWH_SQL",
+    'raise_on_warnings': True
+}
+
+plotDHT(connection_config)
