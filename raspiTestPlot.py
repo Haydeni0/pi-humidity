1a08a1768c7e567de349a65571bb99d7b4bccccd
Haydeni0 <hayden.dorahy@gmail.com>
2021-10-30 12:17:04 +0100

    a

--- raspiTestPlot.py
+++ raspiTestPlot.py
@@ -1,17 +1,27 @@
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
+import datetime
+import pandas as pd
 
-matplotlib.use('Qt5Agg')
+matplotlib.use('TkAgg')
 
 fig = plt.figure()
 
+current_time = datetime.datetime.now()
+start_dtime = current_time - datetime.timedelta(hours=1)
+
+N = 10
+
+t = np.linspace(
+            pd.Timestamp(start_dtime).value, 
+            pd.Timestamp(current_time).value, N)
+t = pd.to_datetime(t)
+
 ax = fig.add_subplot(1, 1, 1)
 
-x = np.arange(0, 10, 0.1)
-y = x
+x = np.linspace(0, 10, N)
 
-ax.plot(x, y)
-print(matplotlib.get_backend())
+ax.plot(t, x)
 plt.show()
 
