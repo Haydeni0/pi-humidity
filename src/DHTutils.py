fb4cff08b90d60566e40740f85387bcdd4151054
Haydeni0 <hayden.dorahy@gmail.com>
2022-11-11 16:10:05 +0000

    Add type hints

--- src/DHTutils.py
+++ src/DHTutils.py
@@ -4,7 +4,15 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def read_retry_inout(sensor, pin_inside, pin_outside, retries=10, delay_seconds=1, platform=None):
+
+def read_retry_inout(
+    sensor,
+    pin_inside: int,
+    pin_outside: int,
+    retries: int = 10,
+    delay_seconds: int = 1,
+    platform=None,
+) -> tuple[float | None, float | None, float | None, float | None]:
     """Read DHT sensor of specified sensor type (DHT11, DHT22, or AM2302) on
     specified pin and return a tuple of humidity (as a floating point value
     in percent) and temperature (as a floating point value in Celsius).
@@ -14,34 +22,49 @@ def read_retry_inout(sensor, pin_inside, pin_outside, retries=10, delay_seconds=
     of (None, None) is returned. The delay between retries is by default 1
     seconds, but can be overridden.
 
-    This has been modified from Adafruit_DHT source to allow reading of two sensors at 
+    This has been modified from Adafruit_DHT source to allow reading of two sensors at
     once without queueing their retries (which adds latency)
     """
     done_inside = False
     done_outside = False
-
-    for i in range(retries):
+    num_retries = 0
+    while not (done_inside and done_outside):
         if not done_inside:
-            humidity_inside, temperature_inside = Adafruit_DHT.read(sensor, pin_inside, platform)
-            if humidity_inside is not None and temperature_inside is not None:
+            inside_H, inside_T = Adafruit_DHT.read(
+                sensor, pin_inside, platform
+            )
+            if inside_H is not None and inside_T is not None:
                 done_inside = True
         if not done_outside:
-            humidity_outside, temperature_outside = Adafruit_DHT.read(sensor, pin_outside, platform)
-            if humidity_outside is not None and temperature_outside is not None:
+            outside_H, outside_T = Adafruit_DHT.read(
+                sensor, pin_outside, platform
+            )
+            if outside_H is not None and outside_T is not None:
                 done_outside = True
 
         if done_inside and done_outside:
-            return (humidity_inside, temperature_inside, humidity_outside, temperature_outside)
-        
-        if i < retries-1:
-            time.sleep(delay_seconds)
+            return (
+                inside_H,
+                inside_T,
+                outside_H,
+                outside_T,
+            )
+
+        if num_retries >= retries:
+            break
+        else:
+            num_retries += 1
 
     if done_inside and not done_outside:
-        logger.debug(f"Logging inside only: H({humidity_inside}) T({temperature_inside})")
-        return (humidity_inside, temperature_inside, None, None)
+        logger.debug(
+            f"Logging inside only: H({inside_H}) T({inside_T})"
+        )
+        return (inside_H, inside_T, None, None)
     elif not done_inside and done_outside:
-        logger.debug(f"Logging outside only: H({humidity_outside}) T({temperature_outside})")
-        return (None, None, humidity_outside, temperature_outside)
+        logger.debug(
+            f"Logging outside only: H({outside_H}) T({outside_T})"
+        )
+        return (None, None, outside_H, outside_T)
     else:
         logger.debug(f"Logging none")
         return (None, None, None, None)
