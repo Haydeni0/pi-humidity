89896a422dcc42569254a4e2e9d20c17823052c2
Haydeni0 <hayden.dorahy@gmail.com>
2022-11-13 18:11:05 +0000

    Fix port

--- src/newDhtPlotter.py
+++ src/newDhtPlotter.py
@@ -68,4 +68,4 @@ def updateGraph(n: int) -> tuple[go.Figure, go.Figure]:
 
 
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port = 8080, debug=True)
+    app.run_server(host="0.0.0.0", port = "8080", debug=True)
