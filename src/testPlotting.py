029dd018d8478fee6432d87005c350efe9f02963
Haydeni0 <hayden.dorahy@gmail.com>
2022-11-19 20:37:00 +0000

    Update test to use print output

--- src/testPlotting.py
+++ src/testPlotting.py
@@ -1,76 +1,47 @@
-import datetime
-
-import plotly.graph_objects as go
-import plotly.express as px
+from DHT_MySQL_interface import DHTConnection
+from Sensors import DHTSensorData
 from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
+import plotly.graph_objects as go
+from flask import Flask
+import numpy as np
+from time import sleep
 
-from DHT_MySQL_interface import DHTConnection, ObsDHT
-
-SCHEMA_NAME = "test"
-TABLE_NAME_inside = "dht_inside"
-TABLE_NAME_outside = "dht_outside"
-
-TABLE_NAME_inside = f"{SCHEMA_NAME}.{TABLE_NAME_inside}"
-TABLE_NAME_outside = f"{SCHEMA_NAME}.{TABLE_NAME_outside}"
 
 GREEN_HEX = "#74A122"
 RED_HEX = "#D3042F"
 
 conn = DHTConnection()
 
-
+server = Flask(__name__)
 app = Dash(name=__name__)
+
+inside_sensor = DHTSensorData(conn, "test.dht_inside")
+outside_sensor = DHTSensorData(conn, "test.dht_outside")
 
 # Update interval in seconds
 update_interval = 2
 
-app.layout = html.Div(
-    children=[
-        html.H1(children="pi-humidity"),
-        html.Div(
-            children="""
-        Humidity and temperature monitoring
-    """
-        ),
-        dcc.Graph(id="humidity-graph"),
-        dcc.Graph(id="temperature-graph"),
-        dcc.Interval(id="update-tick", interval=update_interval * 1000, n_intervals=0),
-    ]
-)
+while True:
+
+    for sensor, name, colour in zip(
+        [inside_sensor, outside_sensor], ["Inside", "Outside"], [GREEN_HEX, RED_HEX]
+    ):
+        inside_sensor.update()
+        outside_sensor.update()
+
+        D = np.array(sensor.D_grid_centres)
+        H = np.array(sensor.H)
+        T = np.array(sensor.T)
+        print(D)
+        print(H)
+        print(T)
 
 
-@app.callback(
-    [Output("humidity-graph", "figure"), Output("temperature-graph", "figure")],
-    Input("update-tick", "n_intervals"),
-)
-def updateGraph(n: int) -> tuple[go.Figure, go.Figure]:
+    
 
-    fig_T = go.Figure()
-    fig_H = go.Figure()
-
-    fig_T.update_yaxes(title_text="Temperature (<sup>o</sup>C)")
-    fig_H.update_yaxes(title_text="Humidity (%RH)")
-
-    end_dtime = datetime.datetime.now()
-    start_dtime = end_dtime - datetime.timedelta(seconds = 20)
-
-    # Inside
-    D, H, T = conn.getObservations(
-        TABLE_NAME_inside, start_dtime=start_dtime, end_dtime=end_dtime
-    )
-    fig_H.add_trace(go.Scatter(x=D, y=H, marker_color=GREEN_HEX, name = "Inside"))
-    fig_T.add_trace(go.Scatter(x=D, y=T, marker_color=GREEN_HEX, name = "Inside"))
-
-    # Outside
-    D, H, T = conn.getObservations(
-        TABLE_NAME_outside, start_dtime=start_dtime, end_dtime=end_dtime
-    )
-    fig_H.add_trace(go.Scatter(x=D, y=H, marker_color=RED_HEX, name = "Outside"))
-    fig_T.add_trace(go.Scatter(x=D, y=T, marker_color=RED_HEX, name = "Outside"))
-
-    return fig_H, fig_T
+    sleep(update_interval)
 
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+
+
