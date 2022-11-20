502d27628aff2c8366ee527869f2f682d535d1d8
Haydeni0 <hayden.dorahy@gmail.com>
2022-11-20 15:33:37 +0000

    Reformat plotter

--- src/dhtPlotter.py
+++ src/dhtPlotter.py
@@ -18,7 +18,7 @@ update_interval = 5
 
 sensor_history = timedelta(minutes=2)
 
-# There are still some major problems with 
+# There are still some major problems with
 # stability when num_grid is too big or small...
 num_grid = 800
 # Define num_grid by the desired update interval
@@ -26,8 +26,12 @@ num_grid = 800
 # print(num_grid)
 # sys.exit()
 
-inside_sensor = SensorData(conn, "test.dht_inside", sensor_history=sensor_history, num_grid=num_grid)
-outside_sensor = SensorData(conn, "test.dht_outside", sensor_history=sensor_history, num_grid=num_grid)
+inside_sensor = SensorData(
+    conn, "test.dht_inside", sensor_history=sensor_history, num_grid=num_grid
+)
+outside_sensor = SensorData(
+    conn, "test.dht_outside", sensor_history=sensor_history, num_grid=num_grid
+)
 
 
 fig_H = go.Figure()
@@ -42,6 +46,7 @@ app.layout = html.Div(
         dcc.Interval(id="update-tick", interval=update_interval * 1000, n_intervals=0),
     ]
 )
+
 
 @app.callback(
     [Output("humidity-graph", "figure"), Output("temperature-graph", "figure")],
@@ -65,12 +70,20 @@ def updateGraph(n: int) -> tuple[dict, dict]:
         H_traces.append(go.Scatter(x=D, y=H, marker_color=colour, name=name))
         T_traces.append(go.Scatter(x=D, y=T, marker_color=colour, name=name))
 
-    H_layout = go.Layout(yaxis=go.layout.YAxis(title = "Humidity (%RH)"), font=go.layout.Font(size=18))
-    T_layout = go.Layout(yaxis=go.layout.YAxis(title = "Temperature (<sup>o</sup>C)"), font=go.layout.Font(size=18))
+    H_layout = go.Layout(
+        yaxis=go.layout.YAxis(title="Humidity (%RH)"), font=go.layout.Font(size=18)
+    )
+    T_layout = go.Layout(
+        yaxis=go.layout.YAxis(title="Temperature (<sup>o</sup>C)"),
+        font=go.layout.Font(size=18),
+    )
 
     # Only update elements of the figure, rather than returning a whole new figure. This is much faster.
-    return {"data": H_traces, "layout": H_layout}, {"data": T_traces, "layout": T_layout}
+    return {"data": H_traces, "layout": H_layout}, {
+        "data": T_traces,
+        "layout": T_layout,
+    }
 
 
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port = "8080", debug=True)
+    app.run_server(host="0.0.0.0", port="8080", debug=True)
