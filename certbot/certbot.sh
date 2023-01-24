ff47b8df7ac6086ef998620861f1c3227d7f7954
Haydeni0 <hayden.dorahy@gmail.com>
2023-01-24 21:56:18 +0000

    Fix cronjob, delete old certbot

--- certbot/certbot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-certbot certonly -d pi-humidity.webredirect.org --email hayden.dorahy@gmail.com --agree-tos --standalone --non-interactive
-chmod -R 777 /etc/letsencrypt
--- src/webserver.py
+++ src/webserver.py
@@ -59,8 +59,9 @@ if __name__ == "__main__":
     # options = gunicornConfig()
     # StandaloneApplication(server, options).run()
 
+    # Set up a cronjob to renew the certificate every day at 0030
     os.system("crontab -l > my_cron")
-    os.system("echo 30 * * * * python /src/my_certbot.py  >> /tmp/my_cron")
+    os.system("echo 30 \* \* \* \* python /src/my_certbot.py  >> /tmp/my_cron")
     os.system("crontab /tmp/my_cron")
     os.system("rm /tmp/my_cron")
 
