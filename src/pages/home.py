da2ba548707aaf088f2ea4491dfd086cfb4a95ad
Haydeni0 <hayden.dorahy@gmail.com>
2023-03-11 11:49:59 +0000

    Add page titles

--- src/pages/home.py
+++ src/pages/home.py
@@ -2,6 +2,6 @@ from dash_extensions.enrich import register_page, html
 import layouts
 
 # Use this page as the default one (path "/")
-register_page(__name__, path="/")
+register_page(__name__, path="/", title="pi-humidity")
 
 layout = layouts.dhtPlotter()
--- src/pages/static-humidity.py
+++ src/pages/static-humidity.py
@@ -1,5 +1,5 @@
 from dash_extensions.enrich import register_page, html
 import layouts
-register_page(__name__)
+register_page(__name__, title="Humidity")
 
 layout = layouts.static_humidity()
