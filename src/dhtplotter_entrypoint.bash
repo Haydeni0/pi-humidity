06fd38807548105ee6443275a7a62f5de41bc6be
Haydeni0 <hayden.dorahy@gmail.com>
2023-04-18 21:43:08 +0100

    Fix entrypoint

--- src/dhtplotter_entrypoint.bash
+++ src/dhtplotter_entrypoint.bash
@@ -7,7 +7,7 @@ printenv | grep -v \"no_proxy\" >> /etc/environment
 # Set up a cronjob to renew the certificate every Sunday at 0230
 # This completely overrides all other cronjobs - shouldn't be a problem as this is running in a container
 # Specify exact path to python for cron
-"echo '30 2 * * 0 /usr/local/bin/python3 /src/my_certbot.py >> /var/log/cron.log 2>&1' >> /tmp/my_cron
+echo '30 2 * * 0 /usr/local/bin/python3 /src/my_certbot.py >> /var/log/cron.log 2>&1' >> /tmp/my_cron
 crontab /tmp/my_cron
 rm /tmp/my_cron
 cron
