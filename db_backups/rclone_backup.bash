a3b38c2b41e9cc152373ef212f0024d2dbfd8971
Haydeni0 <hayden.dorahy@gmail.com>
2023-04-24 19:45:57 +0100

    Add variable for Google Drive path

--- db_backups/rclone_backup.bash
+++ db_backups/rclone_backup.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+GDRIVE_PATH=/backups/pi-humidity/
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ENV_FILE=$SCRIPT_DIR/../postgres.env
 DB_NAME=$(cat $ENV_FILE | grep -Po "(?<=POSTGRES_DB\=)[\w\d]+")
@@ -15,10 +17,10 @@ NEW_DUMP_PATH=$SCRIPT_DIR/${DB_NAME}_week$WEEK_NUMBER_ISO.bak
 # Rename it temporarily
 mv $DUMP_PATH $NEW_DUMP_PATH
 
-echo Copying \"$NEW_DUMP_PATH\" to \"my_gdrive:/backups/pi-humidity/\"
+echo Copying \"$NEW_DUMP_PATH\" to \"my_gdrive:$GDRIVE_PATH\"
 /usr/bin/rclone copy \
     --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s \
-    "$NEW_DUMP_PATH" "my_gdrive:/backups/pi-humidity/"
+    "$NEW_DUMP_PATH" "my_gdrive:$GDRIVE_PATH"
 
 # Restore the original name
 mv $NEW_DUMP_PATH $DUMP_PATH
