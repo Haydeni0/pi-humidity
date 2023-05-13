11ec2752393f39239b10f9cb477becd05f94eeeb
Haydeni0 <hayden.dorahy@gmail.com>
2023-05-13 12:26:29 +0100

    Use dht22 print method

--- src/cdhtlogger/dhtLogger.cpp
+++ src/cdhtlogger/dhtLogger.cpp
@@ -1,10 +1,11 @@
 /*
-Compile and run with 
-    g++ src/cdhtlogger/dhtLogger.cpp src/cdhtlogger/dht22/dht22lib.cpp -lwiringPi -o dhtLogger.exe && ./dhtLogger.exe
+Compile and run with
+    g++ src/cdhtlogger/dhtLogger.cpp src/cdhtlogger/dht22/dht22lib.cpp -lwiringPi -o dhtLogger.exe
+&& ./dhtLogger.exe
 */
 
-
 #include "dht22/dht22lib.h"
+#include <vector>
 
 int main(void)
 {
@@ -13,16 +14,22 @@ int main(void)
         exit(1);
     }
 
-    DhtSensor sensors[2] = {23, 25};
+    std::vector<DhtSensor> sensors = {23, 25};
+
+#ifdef DEBUG
+    DhtSensor::printSignalTitle();
+#endif
 
     int delayMilliseconds = 500;
     for (int i = 0; i < 1000; i++) {
         for (DhtSensor sensor : sensors) {
             sensor.read();
-            printf("%-3.1f *C  Humidity: %-3.1f%%\n", sensor.m_temperature, sensor.m_humidity);
+#ifdef DEBUG
+            sensor.printSignal();
+#endif
+            sensor.print();
+            delay(delayMilliseconds/sensors.size());// Wait between readings
         }
-
-        delay(delayMilliseconds);  // Wait between readings
     }
 
     return (0);
