fddde17bf95de71aad0a399044bb461b1798361d
Haydeni0 <hayden.dorahy@gmail.com>
2023-07-20 18:51:20 +0100

    Fix SSL certificate renewal cronjob

--- src/dhtplotter_entrypoint.bash
+++ src/dhtplotter_entrypoint.bash
@@ -9,7 +9,7 @@ printenv | grep -v \"no_proxy\" >> /etc/environment
 # Renew the SSL certificate every Sunday at 0230
 echo '30 2 * * 0 /usr/local/bin/python3 /src/my_certbot.py >> /var/log/cron.log 2>&1' > /tmp/my_cron
 # Generate static images of the graphs every 10 minutes
-echo '*/10 * * * * /usr/local/bin/python3 /src/dhtPlotterStatic.py' > /tmp/my_cron
+echo '*/10 * * * * /usr/local/bin/python3 /src/dhtPlotterStatic.py' >> /tmp/my_cron
 crontab /tmp/my_cron
 rm /tmp/my_cron
 cron
